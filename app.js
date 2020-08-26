b480fac6b766fbcd158f735fe1f737e670641483
prasannatuladhar <prasanna.pmt@gmail.com>
2020-08-26 21:41:26 +0545

    object literals

--- app.js
+++ app.js
@@ -77,3 +77,55 @@ console.log(friendsToMeet)
 //removing beggining of array
 friendsToMeet.shift()
 console.log(friendsToMeet)
+
+//concat
+let softdrinks=["coke","fanta","dew"]
+let harddrinks =["vodka","gin","beer"]
+drinks=softdrinks.concat(harddrinks)
+console.log(drinks)
+
+//includes 
+let food=["momo","chowmein","chicken","noodles"]
+console.log(food.includes("soup"));
+
+//indexOf
+console.log(food.indexOf("chicken"))
+//checing using if else
+if (food.indexOf("rice")===-1){
+    console.log("Nepalase cant survive")
+}
+
+//reverse
+let name=["P","R","A","S","A","N","N","A"]
+console.log(name.reverse());
+
+//join
+console.log(name.reverse().join("-"))
+
+//Slice
+console.log(food.slice(1,3))
+
+//Splice
+name.splice(0,0,'E','R','.');
+console.log(name);
+
+//using const with array
+const hero=["chaptain America","Spider man","hulk","superman","batman"]
+hero.push("IronMan")
+hero.unshift("AntMan")
+console.log(hero)
+
+//Creating a object literals
+const info ={
+    name:'prasanna',
+    age:26,
+    address:'Nepal',
+    strong:true,
+    1 : 'travelling',
+
+}
+console.log(info.name)
+console.log(info['1'])
+info['hobby']='gaming'
+info.lazy=false
+console.log(info)
